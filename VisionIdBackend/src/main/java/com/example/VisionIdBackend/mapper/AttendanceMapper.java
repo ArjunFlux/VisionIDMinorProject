d54b4fe2492758f67d0fead5a0bd3f0fdbbce0a8
Kartikeya <kartikeyayash4@gmail.com>
2026-05-06 22:58:49 +0530

    Attendance percentage to 2 decimal places

--- VisionIdBackend/src/main/java/com/example/VisionIdBackend/mapper/AttendanceMapper.java
+++ VisionIdBackend/src/main/java/com/example/VisionIdBackend/mapper/AttendanceMapper.java
@@ -6,22 +6,28 @@ import com.example.VisionIdBackend.dto.attendanceDtos.StudentAttendanceDto;
 import com.example.VisionIdBackend.entity.AttendanceEntity;
 import com.example.VisionIdBackend.entity.ClassEntity;
 import com.example.VisionIdBackend.entity.StudentEntity;
+
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AttendanceMapper {
 
-    public static StudentAttendanceDto toStudentAttendanceDto(StudentEntity student,double percentage) {
+    public static StudentAttendanceDto toStudentAttendanceDto(StudentEntity student, double percentage) {
 
         StudentAttendanceDto attendanceDto = new StudentAttendanceDto();
 
         attendanceDto.setName(student.getName());
         attendanceDto.setRollNo(student.getRollNo());
+
+        double value = 0.12452;
+
+        percentage = percentage * 100;
+        percentage = Math.round(percentage * 100.0) / 100.0;
         attendanceDto.setAttendance_percentage(percentage);
 
         return attendanceDto;
     }
-
 
 
 }
